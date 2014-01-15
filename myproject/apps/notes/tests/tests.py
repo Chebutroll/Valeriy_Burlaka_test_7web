13abcdbaf8b09a9e38a820b32203d37f5b1e8846
Valeriy.Burlaka <chebusun@gmail.com>
2014-01-15 16:14:11 +0200

    added tests for notes index view

--- myproject/apps/notes/tests/tests.py
+++ myproject/apps/notes/tests/tests.py
@@ -1,8 +1,20 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 3)
+
+class NotesViewsTestCase(TestCase):
+    fixtures = ['notes_views_testdata.json']
+
+    def test_index_redirect(self):
+        response = self.client.get('/')
+        self.assertRedirects(response, reverse('notes:index_page'))
+
+    def test_index_page(self):
+        response = self.client.get(reverse('notes:index_page'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('notes_list' in response.context)
+        self.assertEqual(len(response.context['notes_list']), 5)
+        self.assertTemplateUsed(response, 'notes/notes_base.html')
+        self.assertTemplateUsed(response, 'notes/notes_index.html')
+        note_1 = response.context['notes_list'][0]
+        self.assertEqual(note_1.body_text, 'This is a text for text note')
