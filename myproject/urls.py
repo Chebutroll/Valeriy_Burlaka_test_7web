4a33d90af4077534a9aca87b0449c998b5e99b4f
Valeriy.Burlaka <chebusun@gmail.com>
2014-01-26 17:48:29 +0200

    changed template-tag test to use WebTest; fixed > 79-character-lines

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,7 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'myproject.apps.notes.views.index_redirect', name='index_redirect'),
-    url(r'^notes/', include('myproject.apps.notes.urls', namespace='notes', app_name='notes')),
+    url(r'^$', 'myproject.apps.notes.views.index_redirect',
+        name='index_redirect'),
+    url(r'^notes/', include('myproject.apps.notes.urls',
+                            namespace='notes',
+                            app_name='notes')),
     url(r'^admin/', include(admin.site.urls)),
 )
